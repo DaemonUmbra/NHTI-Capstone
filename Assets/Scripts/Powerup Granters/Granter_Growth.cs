641819652ab60bd7e12b1935761ac3d38d671f51
DaemonUmbra <daemon.umbra@gmail.com>
2017-11-21 17:59:00 -0500

    Added Growth Ability and Growth Granter, suggest overhaul of Ability System

---
+++ Assets/Scripts/Powerup Granters/Granter_Growth.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace Powerup_Granters
+{
+    [RequireComponent(typeof(Collider))]
+    public class Granter_Growth : MonoBehaviour
+    {
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        private void OnCollisionEnter(Collision collision)
+        {
+            if (collision.gameObject.tag == "Player")
+            {
+                collision.gameObject.GetComponent<AbilityManager>().AddAbility<Powerups.Powerup_Growth>();
+                Destroy(gameObject);
+            }
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.gameObject.tag == "Player")
+            {
+                other.gameObject.GetComponent<AbilityManager>().AddAbility<Powerups.Powerup_Growth>();
+                Destroy(gameObject);
+            }
+        }
+
+    }
+}
---
+++ Assets/Scripts/Powerups/Powerup_Growth.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Powerups
+{
+    public class Powerup_Growth : Ability
+    {
+        private Vector3 OriginalScale;
+        public float GrowthFactor = 2;
+        public override void OnAbilityAdd()
+        {
+            OriginalScale = transform.localScale;
+            transform.localScale = OriginalScale * GrowthFactor;
+        }
+
+        public override void OnUpdate()
+        {
+
+        }
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        public override void OnAbilityRemove()
+        {
+
+            transform.localScale = OriginalScale;
+        }
+    }
+}
