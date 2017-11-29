e63a2ccf82098f67cc781ae687e22ef5e191df38
bnelligan <nelligan81@gmail.com>
2017-11-28 19:32:35 -0500

    Hit Effect System and DamageType base
    Added a basic on hit effect system and fixed the damagetype class

--- Assets/Scripts/Damage Types/DamageType.cs
+++ Assets/Scripts/Damage Types/DamageType.cs
@@ -6,7 +6,18 @@ using UnityEngine;
 /// Base damage type class. 
 /// Derive all damage type classes from this.
 /// </summary>
-public class DamageType : ScriptableObject {
+public abstract class DamageType {
 
-	
+    // Effects passed to the target
+    List<Effect> OnHitEffects;
+
+    // Call this function when a player takes damage. Pass in the hit character.
+    public virtual void ApplyDamageType(GameObject target)
+    {
+        // Applies the on hit effects to the target
+        foreach (Effect e in OnHitEffects)
+        {
+            e.ApplyEffect(target);
+        }
+    }
 }
