60e010520a3158a2bf8f1634dbbc9cd630733791
IanFC <33268732+IanFC@users.noreply.github.com>
2017-11-28 19:21:42 -0500

    Snipe
    marker conflicts

--- Assets/Scripts/Snipe.cs
+++ Assets/Scripts/Snipe.cs
@@ -17,49 +17,42 @@ Information
   */
 ///summary
 
-public class Snipe : Ability
+namespace Powerups
 {
-<<<<<<< HEAD
     public class Snipe : ActiveAbility
     {
-        //private float remainingCD;
+        private float CDstart;
         bool onCooldown = false, CurrentlyActive = false;
         PlayerShoot pShoot;
-=======
-    float abilityTime;
-    bool onCooldown = false, CurrentlyActive = false;
-    PlayerShoot pShoot;
-    
->>>>>>> 1478560bf921b35d94462835ab3ae68cc7d1c513
 
-    public override void OnAbilityAdd()
-    {
-        // Set name
-        Name = "Snipe";
-        Debug.Log(Name + " Added");
 
-        // Add new shoot function to delegate 
-        pShoot = GetComponent<PlayerShoot>();
-        if (pShoot)
+        public override void OnAbilityAdd()
         {
-            Debug.Log("Snipe Added to Shoot Delegate");
-            pShoot.shoot += OnShoot;
-        }
-    }
-    public override void OnUpdate()
-    {
-        
-    }
-    public override void OnAbilityRemove()
-    {
-        // Remove shoot delegate
-        if (pShoot)
-        {
-            pShoot.shoot -= OnShoot;
-        }
-        pShoot = null;
+            // Set name
+            Name = "Snipe";
+            Debug.Log(Name + " Added");
 
-<<<<<<< HEAD
+            // Add new shoot function to delegate 
+            pShoot = GetComponent<PlayerShoot>();
+            if (pShoot)
+            {
+                Debug.Log("Snipe Added to Shoot Delegate");
+                pShoot.shoot += OnShoot;
+            }
+        }
+        public override void OnUpdate()
+        {
+
+        }
+        public override void OnAbilityRemove()
+        {
+            // Remove shoot delegate
+            if (pShoot)
+            {
+                pShoot.shoot -= OnShoot;
+            }
+            pShoot = null;
+
             // Call base function
             base.OnAbilityRemove();
         }
@@ -68,67 +61,57 @@ public class Snipe : Ability
             throw new NotImplementedException();
         }
         public void OnShoot()
-=======
-        // Call base function
-        base.OnAbilityRemove();
-    }
-    public void OnShoot()
-    {
-        if (onCooldown)
->>>>>>> 1478560bf921b35d94462835ab3ae68cc7d1c513
         {
-            return;
+            if (onCooldown)
+            {
+                Debug.Log("This ability is on cooldown" + "Start Time: " + CDstart);
+                return;
+            }
+            //Debug.Log("Raycast Shot");
+            GameObject rayOrigin = GameObject.Find("Player/Gun"); //Needs to be changed to local player when networked
+            Vector3 mp = Input.mousePosition;
+            mp.z = 10;
+            Vector3 mouseLocation = Camera.main.ScreenToWorldPoint(mp);
+            Vector3 targetVector = mouseLocation;
+
+            Ray snipeRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            RaycastHit endPoint;
+
+            if (Physics.Raycast(snipeRay, out endPoint))
+            {
+                Debug.Log(endPoint.transform.gameObject.name);
+                targetVector = endPoint.point;
+
+            }
+            else
+            {
+                Debug.Log("no object was hit");
+            }
+
+
+            StartCoroutine(VisualizeRaycast(rayOrigin, targetVector));
         }
-        //Debug.Log("Raycast Shot");
-        GameObject rayOrigin = GameObject.Find("Player/Gun"); //Needs to be changed to local player when networked
-        Vector3 mp = Input.mousePosition;
-        mp.z = 10;
-        Vector3 mouseLocation = Camera.main.ScreenToWorldPoint(mp);
-        Vector3 targetVector = mouseLocation;
-
-        Ray snipeBolt = Camera.main.ScreenPointToRay(Input.mousePosition);
-
-        RaycastHit endPoint;
-
-        if (Physics.Raycast(snipeBolt, out endPoint))
-        {
-            Debug.Log(endPoint.transform.gameObject.name);
-            targetVector = endPoint.point;
-
-        }
-<<<<<<< HEAD
         IEnumerator VisualizeRaycast(GameObject Origin, Vector3 targetLocation)
         {
             StartCoroutine(TriggerCoolDown());
-=======
-        //Debug.DrawRay(rayOrigin.transform.position, mouseLocation - rayOrigin.transform.position, Color.red, 5.0f);
->>>>>>> 1478560bf921b35d94462835ab3ae68cc7d1c513
 
-        StartCoroutine(VisualizeRaycast(rayOrigin, targetVector));
-    }
-    IEnumerator VisualizeRaycast(GameObject Origin, Vector3 targetLocation)
-    {
-        onCooldown = true;
+            LineRenderer snipeLaser = Origin.GetComponent<LineRenderer>();
+            snipeLaser.SetPosition(0, Origin.transform.position);
+            snipeLaser.SetPosition(1, targetLocation);
 
-        LineRenderer snipeLaser = Origin.GetComponent<LineRenderer>();
-        snipeLaser.SetPosition(0, Origin.transform.position);
-        snipeLaser.SetPosition(1, targetLocation);
+            snipeLaser.enabled = true;
+            yield return new WaitForSeconds(.2f);
+            snipeLaser.enabled = false;
 
-<<<<<<< HEAD
         }
         IEnumerator TriggerCoolDown()
         {
             onCooldown = true;
+            CDstart = Time.fixedTime;
             yield return new WaitForSeconds(5);
             onCooldown = false;
         }
-=======
-        snipeLaser.enabled = true;
-        yield return new WaitForSeconds(.5f);
-        snipeLaser.enabled = false;
->>>>>>> 1478560bf921b35d94462835ab3ae68cc7d1c513
 
-        onCooldown = false;
     }
-
 }
