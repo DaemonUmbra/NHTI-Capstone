04119912543198cf232a7ac89e262b1c4e38131c
zikiro <mrzombiekilla24@yahoo.com>
2017-11-28 20:30:49 -0500

    Update to the Network Lobby
    Network Lobby push

--- Assets/Scripts/Snipe.cs
+++ Assets/Scripts/Snipe.cs
@@ -4,96 +4,97 @@ using System.Collections.Generic;
 using UnityEngine;
 
 ///summary
- /*
+/*
 Developers and Contributors: Ian Cahoon
-    
+
 Information
-    Name: Snipe
-    Type: Active
-    Effect: Zoom in for AbilityTime(float), for the duration of the ability, your basic projectiles become hitscan
-            Lower fire rate, higher damage, and a cool lightning zappy zap effect
-    Tier: Rare
-    Description: Basic fire turns into hitscan lightning bolts with increase damage but decreased fire rate
-  */
+   Name: Snipe
+   Type: Active
+   Effect: Zoom in for AbilityTime(float), for the duration of the ability, your basic projectiles become hitscan
+           Lower fire rate, higher damage, and a cool lightning zappy zap effect
+   Tier: Rare
+   Description: Basic fire turns into hitscan lightning bolts with increase damage but decreased fire rate
+
 ///summary
 
 public class Snipe : Ability
 {
-    float abilityTime;
-    bool onCooldown = false, CurrentlyActive = false;
-    PlayerShoot pShoot;
-    
+   float abilityTime;
+   bool onCooldown = false, CurrentlyActive = false;
+   PlayerShoot pShoot;
 
-    public override void OnAbilityAdd()
-    {
-        // Set name
-        Name = "Snipe";
-        Debug.Log(Name + " Added");
 
-        // Add new shoot function to delegate 
-        pShoot = GetComponent<PlayerShoot>();
-        if (pShoot)
-        {
-            Debug.Log("Snipe Added to Shoot Delegate");
-            pShoot.shoot += OnShoot;
-        }
-    }
-    public override void OnUpdate()
-    {
-        
-    }
-    public override void OnAbilityRemove()
-    {
-        // Remove shoot delegate
-        if (pShoot)
-        {
-            pShoot.shoot -= OnShoot;
-        }
-        pShoot = null;
+   public override void OnAbilityAdd()
+   {
+       // Set name
+       Name = "Snipe";
+       Debug.Log(Name + " Added");
 
-        // Call base function
-        base.OnAbilityRemove();
-    }
-    public void OnShoot()
-    {
-        if (onCooldown)
-        {
-            return;
-        }
-        //Debug.Log("Raycast Shot");
-        GameObject rayOrigin = GameObject.Find("Player/Gun"); //Needs to be changed to local player when networked
-        Vector3 mp = Input.mousePosition;
-        mp.z = 10;
-        Vector3 mouseLocation = Camera.main.ScreenToWorldPoint(mp);
-        Vector3 targetVector = mouseLocation;
+       // Add new shoot function to delegate 
+       pShoot = GetComponent<PlayerShoot>();
+       if (pShoot)
+       {
+           Debug.Log("Snipe Added to Shoot Delegate");
+           pShoot.shoot += OnShoot;
+       }
+   }
+   public override void OnUpdate()
+   {
 
-        Ray snipeBolt = Camera.main.ScreenPointToRay(Input.mousePosition);
+   }
+   public override void OnAbilityRemove()
+   {
+       // Remove shoot delegate
+       if (pShoot)
+       {
+           pShoot.shoot -= OnShoot;
+       }
+       pShoot = null;
 
-        RaycastHit endPoint;
+       // Call base function
+       base.OnAbilityRemove();
+   }
+   public void OnShoot()
+   {
+       if (onCooldown)
+       {
+           return;
+       }
+       //Debug.Log("Raycast Shot");
+       GameObject rayOrigin = GameObject.Find("Player/Gun"); //Needs to be changed to local player when networked
+       Vector3 mp = Input.mousePosition;
+       mp.z = 10;
+       Vector3 mouseLocation = Camera.main.ScreenToWorldPoint(mp);
+       Vector3 targetVector = mouseLocation;
 
-        if (Physics.Raycast(snipeBolt, out endPoint))
-        {
-            Debug.Log(endPoint.transform.gameObject.name);
-            targetVector = endPoint.point;
+       Ray snipeBolt = Camera.main.ScreenPointToRay(Input.mousePosition);
 
-        }
-        //Debug.DrawRay(rayOrigin.transform.position, mouseLocation - rayOrigin.transform.position, Color.red, 5.0f);
+       RaycastHit endPoint;
 
-        StartCoroutine(VisualizeRaycast(rayOrigin, targetVector));
-    }
-    IEnumerator VisualizeRaycast(GameObject Origin, Vector3 targetLocation)
-    {
-        onCooldown = true;
+       if (Physics.Raycast(snipeBolt, out endPoint))
+       {
+           Debug.Log(endPoint.transform.gameObject.name);
+           targetVector = endPoint.point;
 
-        LineRenderer snipeLaser = Origin.GetComponent<LineRenderer>();
-        snipeLaser.SetPosition(0, Origin.transform.position);
-        snipeLaser.SetPosition(1, targetLocation);
+       }
+       //Debug.DrawRay(rayOrigin.transform.position, mouseLocation - rayOrigin.transform.position, Color.red, 5.0f);
 
-        snipeLaser.enabled = true;
-        yield return new WaitForSeconds(.5f);
-        snipeLaser.enabled = false;
+       StartCoroutine(VisualizeRaycast(rayOrigin, targetVector));
+   }
+   IEnumerator VisualizeRaycast(GameObject Origin, Vector3 targetLocation)
+   {
+       onCooldown = true;
 
-        onCooldown = false;
-    }
+       LineRenderer snipeLaser = Origin.GetComponent<LineRenderer>();
+       snipeLaser.SetPosition(0, Origin.transform.position);
+       snipeLaser.SetPosition(1, targetLocation);
+
+       snipeLaser.enabled = true;
+       yield return new WaitForSeconds(.5f);
+       snipeLaser.enabled = false;
+
+       onCooldown = false;
+   }
 
 }
+*/
