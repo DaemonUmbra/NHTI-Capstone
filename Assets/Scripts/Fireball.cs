e1aebebedaafcf3ae4e7276641168e32c3fa6741
IanFC <33268732+IanFC@users.noreply.github.com>
2017-11-28 20:55:43 -0500

    snipe/slime
    added slime using original snipe code, as well as new materials for slime and snipe

---
+++ Assets/Scripts/Fireball.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Powerups
+{
+    public class Fireball : MonoBehaviour
+    {
+        public float health = 10.0f;
+        int count = 0;
+
+        public void OnTriggerEnter(Collider collision)
+        {
+            if (count != 0)
+            {
+                count = 0;
+                InvokeRepeating("Damage", 0.0f, 1.0f);
+            }
+            Destroy(collision.gameObject);
+        }
+        void Damage()
+        {
+            if (count != 4)
+            {
+                health -= 0.5f;
+                count++;
+            }
+            else
+            {
+                return;
+            }
+        }
+    }
+}
