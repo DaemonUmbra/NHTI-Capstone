067c78d80e72b120857523a28be638f2f2f77ebd
blaj95 <32546217+blaj95@users.noreply.github.com>
2018-02-02 12:36:21 -0500

    BasicNetworked
    players joining same room

---
+++ Assets/Scripts/BrodeyNetworking/HUD.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+namespace PUNTutorial
+{
+    public class HUD : MonoBehaviour
+    {
+        static HUD instance;
+
+        void Awake()
+        {
+            if (instance != null)
+            {
+                DestroyImmediate(gameObject);
+                return;
+            }
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+
+    }
+}
