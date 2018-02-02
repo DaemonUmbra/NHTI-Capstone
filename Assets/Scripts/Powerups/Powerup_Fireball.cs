0540c71542582e25c3deff2ba75bbae89973f8b1
EmberCelicaX <33268735+EmberCelicaX@users.noreply.github.com>
2018-02-02 12:02:22 -0500

    More work on the Fireball weapon

---
+++ Assets/Scripts/Powerups/Powerup_Fireball.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Powerup_Fireball : ActiveAbility
+{
+    PlayerShoot pShoot;
+
+    public override void OnAbilityAdd()
+    {
+        Name = "Fireball";
+        Debug.Log(Name + " Added");
+
+        pShoot = GetComponent<PlayerShoot>();
+        if (pShoot)
+        {
+            Debug.Log("Fireball Added to Shoot Delegate");
+            pShoot.shoot += OnShoot;
+        }
+    }
+
+    public override void OnAbilityRemove()
+    {
+        // Remove shoot delegate
+        if (pShoot)
+        {
+            pShoot.shoot -= OnShoot;
+        }
+        pShoot = null;
+
+        // Call base function
+        base.OnAbilityRemove();
+    }
+
+    public override void OnUpdate()
+    {
+
+    }
+
+    public override void Activate()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void OnShoot()
+    {
+        
+    }
+}
