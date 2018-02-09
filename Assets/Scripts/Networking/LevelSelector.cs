29a31f931579a3b959235f8a7e68bb13484e5dcb
Ian MacKenzie <32595467+Shoebopadoo@users.noreply.github.com>
2018-02-09 14:23:14 -0500

    Map selection works
    You can now select maps and it will show the preview and the desciption.
    Next I am going to try and Automate the process

--- Assets/Scripts/Networking/LevelSelector.cs
+++ Assets/Scripts/Networking/LevelSelector.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class LevelSelector : MonoBehaviour
 {
@@ -13,7 +14,14 @@ public class LevelSelector : MonoBehaviour
 	// Use this for initialization
 	void Start ()
     {
-		
+        if (!maps.Contains(null))
+        {
+            Debug.Log("Maps is not empty");
+        }
+        else
+        {
+            Debug.Log("Maps is empty, you silly");
+        }
 	}
 	
 	// Update is called once per frame
@@ -24,17 +32,39 @@ public class LevelSelector : MonoBehaviour
 
     public void ButtonHover(Button button)
     {
-        if(button.name == "Map1")
+        if(button.name == "Button1")
         {
             maps[0].transform.SetAsLastSibling();
+            desciptionText.text = "I AM RED";
         }
-        if (button.name == "Map2")
+        if (button.name == "Button2")
         {
             maps[1].transform.SetAsLastSibling();
+            desciptionText.text = "NOW I AM AN ORANGE";
         }
-        if (button.name == "Map3")
+        if (button.name == "Button3")
         {
             maps[2].transform.SetAsLastSibling();
+            desciptionText.text = "Purple is MY favorite color";
+        }
+    }
+
+    public void ButtonClick(Button button)
+    {
+        if (button.name == "Button1")
+        {
+            //SceneManager.LoadScene("YourLevel here");
+            Debug.Log("Button 1 Clicked");
+        }
+        if (button.name == "Button2")
+        {
+            //SceneManager.LoadScene("Your Level 2 Here");
+            Debug.Log("Button 2 Clicked");
+        }
+        if (button.name == "Button3")
+        {
+            //SceneManager.LoadScene("Your Level 3 here");
+            Debug.Log("Button 3 Clicked");
         }
     }
 }
