3666f4969090d9c0d453290d6c17e1f55a52a695
bnelligan <nelligan81@gmail.com>
2018-02-13 15:15:01 -0500

    Disable on load
    Added a script to disable components from a list when the player is not local

---
+++ Assets/Scripts/Networking/DisableOnLoad.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisableOnLoad : Photon.MonoBehaviour {
+
+    /// <summary>
+    /// Scripts to be disabled for non-local players in Multiplayer
+    /// </summary>
+    public List<MonoBehaviour> ComponentsToDisable;
+    /// <summary>
+    /// Game objects to be disabled by non-local players in Multiplayer
+    /// </summary>
+    public List<GameObject> GameObjectsToDisable;
+
+    
+	void Awake () {
+        if(!photonView.isMine)
+        {
+            // Disable listed scripts
+            if(ComponentsToDisable != null)
+            {
+                foreach(MonoBehaviour script in ComponentsToDisable)
+                {
+                    script.enabled = false;
+                }
+            }
+            // Disable listed GameObjects
+            if(GameObjectsToDisable != null)
+            {
+                foreach(GameObject obj in GameObjectsToDisable)
+                {
+                    obj.SetActive(false);
+                }
+            }
+        }
+	}
+}
