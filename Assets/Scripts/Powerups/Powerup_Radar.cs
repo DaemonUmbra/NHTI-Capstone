77fbf7b76edf8611f4e0e86c7e645b978cfaacaa
Steve <thesteveoninja@gmail.com>
2018-02-16 12:50:49 -0500

    Cleaned Up/Noted Code

--- Assets/Scripts/Powerups/Powerup_Radar.cs
+++ Assets/Scripts/Powerups/Powerup_Radar.cs
@@ -2,9 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Powerup_Radar : MonoBehaviour {
+public class Powerup_Radar : PassiveAbility {
 
-    /*
+ /*
  * Programmer Assigned: Steven Zachary
  * Power-up: Radar
  * Description: A soft beep, beeps more urgently as you approach a rare or higher powerup.                
@@ -14,9 +14,21 @@ public class Powerup_Radar : MonoBehaviour {
     void Start () {
 		
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    public override void OnAbilityAdd() // Function for adding ability to player
+    {
+        Name = "Radar";
+        Debug.Log(Name + " added!");
+        base.OnAbilityAdd();
+    }
+
+    public override void OnAbilityRemove() // Function for removing ability from player
+    {
+        base.OnAbilityRemove();
+    }
+
+    public override void OnUpdate() // Update function
+    {
+        base.OnUpdate();
+    }
 }
