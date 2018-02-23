eac154963c2ea7aac354cdb200273c4e62933883
Steve <thesteveoninja@gmail.com>
2018-02-23 14:23:36 -0500

    Working on Radar (view Description for details)
    Code in place to find closest power up and set audio source volume based on distance

--- Assets/Scripts/Powerups/Powerup_Radar.cs
+++ Assets/Scripts/Powerups/Powerup_Radar.cs
@@ -14,11 +14,18 @@ namespace Powerups
         public AudioSource audioSource;
         public AudioClip bleep; // The bleeps, the sweeps and the creeps
 
+        public float DistanceCheck;
+        public float DistanceToTarget;
+        public int ElementsInArray;
+        public int Closest;
+
         // Use this for initialization
         private void Start()
         {
             pickUp = FindObjectsOfType<Pickup>();
             audioSource = GetComponent<AudioSource>();
+            ElementsInArray = pickUp.Length;
+            Closest = 0;
         }
 
         public override void OnAbilityAdd() // Function for adding ability to player
@@ -35,6 +42,39 @@ namespace Powerups
 
         public override void OnUpdate() // Update function
         {
+        
+            for(int i = 0; i < ElementsInArray; i++) // Find the object closest to the player
+            {
+                if(Vector3.Distance(transform.position, pickUp[i].transform.position) < Vector3.Distance(transform.position, pickUp[Closest].transform.position))
+                {
+                    Closest = i;
+                }
+            }
+
+            DistanceToTarget = Vector3.Distance(transform.position, pickUp[Closest].transform.position); // Sets Distance to the closest power up every frame
+
+            // ** Set volume based on distance from closest power up. The further away the quieter the volume
+
+            if(DistanceToTarget > 50)
+            {
+                audioSource.volume = 25f;
+            }
+
+            else if(DistanceToTarget < 50 && DistanceToTarget > 25)
+            {
+                audioSource.volume = 50f;
+            }
+
+            else if(DistanceToTarget < 25 && DistanceToTarget > 10)
+            {
+                audioSource.volume = 75f;
+            }
+
+            else if(DistanceToTarget < 10)
+            {
+                audioSource.volume = 100f;
+            }
+
             base.OnUpdate();
         }
     }
