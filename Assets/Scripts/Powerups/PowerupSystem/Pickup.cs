e1defd5cc6e9d1a5b1e0537b1d15134f263e381f
blaj95 <32546217+blaj95@users.noreply.github.com>
2018-02-23 16:10:38 -0500

    random powerups on pick
    ^

--- Assets/Scripts/Powerups/PowerupSystem/Pickup.cs
+++ Assets/Scripts/Powerups/PowerupSystem/Pickup.cs
@@ -1,4 +1,8 @@
 ﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 using Powerups;
 
 public class Pickup : Photon.MonoBehaviour
@@ -6,19 +10,57 @@ public class Pickup : Photon.MonoBehaviour
     private AbilityManager aMan;
     private BaseAbility _ability;
     public GameObject spawner;
-    
+    //public List<BaseAbility> powerList;
+    public string[] powerList;
+    public List<string> Powers;
     // Use this for initialization
     private void Start()
     {
-        _ability = GetComponent<BaseAbility>();
-        
+        //_ability = GetComponent<BaseAbility>();
+        //aMan = GameObject.FindGameObjectWithTag("Player").GetComponent<AbilityManager>();
+        //List<KeyValuePair<string, BaseAbility>> list = aMan.ListAbilities().ToList();
+        //foreach(KeyValuePair<string,BaseAbility> power in list)
+        //{
+        //    Debug.Log(power);
+        //}
+        List<string> AvailablePowerupStrings = new List<string>();
+        Type[] Types = System.Reflection.Assembly.GetExecutingAssembly().GetTypes();
+        Dictionary<string, Type> AbilityDict = new Dictionary<string, Type>();
+        foreach (Type type in Types)
+        {
+            if (type.Namespace == "Powerups" && type.IsSubclassOf(typeof(BaseAbility)))
+            {
+                AvailablePowerupStrings.Add(type.Name);
+                AbilityDict.Add(type.Name, type);
+            }
+        }
+
     }
 
     private void Awake()
     {
+        
         if(gameObject != null)
         {
-            gameObject.AddComponent<PowerUp_Blink>();
+            var rnd = new System.Random();
+
+            List<string> AvailablePowerupStrings = new List<string>();
+            Type[] Types = System.Reflection.Assembly.GetExecutingAssembly().GetTypes();
+            Dictionary<string, Type> AbilityDict = new Dictionary<string, Type>();
+            foreach (Type type in Types)
+            {
+                if (type.Namespace == "Powerups" && type.IsSubclassOf(typeof(BaseAbility)))
+                {
+                    AvailablePowerupStrings.Add(type.Name);
+                    AbilityDict.Add(type.Name, type);
+                }
+            }
+            Powers = AvailablePowerupStrings;
+            string powerName = Powers.OrderBy(s => Guid.NewGuid()).First();
+            Debug.Log(powerName);
+            
+            UnityEngineInternal.APIUpdaterRuntimeServices.AddComponent(this.gameObject,"Assets/Scripts/Powerups" ,powerName);
+            
         }
     }
 
