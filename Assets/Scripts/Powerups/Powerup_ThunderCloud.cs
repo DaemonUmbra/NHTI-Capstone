212691f41e79346d170937b34755f849c933c79e
EmberCelicaX <33268735+EmberCelicaX@users.noreply.github.com>
2018-02-23 15:49:31 -0500

    ThunderCloud powerup

---
+++ Assets/Scripts/Powerups/Powerup_ThunderCloud.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Powerup_ThunderCloud : PassiveAbility
+{
+    PlayerStats ps;
+    private float timeLimit = 30.0f;
+    private float timer;
+    public override void OnAbilityAdd()
+    {
+        ps = GetComponentInParent<PlayerStats>();
+
+        ps.WalkSpeed += 1.0f;
+        ps.JumpPower += 1.0f;
+
+        base.OnAbilityAdd();
+    }
+
+    public override void OnAbilityRemove()
+    {
+        ps = GetComponentInParent<PlayerStats>();
+
+        ps.WalkSpeed -= 1.0f;
+        ps.JumpPower -= 1.0f;
+
+        base.OnAbilityRemove();
+    }
+
+    private void Update()
+    {
+        timer += Time.deltaTime;
+        if(timer >= timeLimit)
+        {
+            OnAbilityRemove();
+        }
+    }
+}
