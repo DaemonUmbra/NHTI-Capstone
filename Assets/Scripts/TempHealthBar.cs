cefb5ce38a230cbe1dcbf454ce26177d24e0487f
zikiro <mrzombiekilla24@yahoo.com>
2018-02-27 16:20:53 -0500

    Player UI
    Created Player UI concept

---
+++ Assets/Scripts/TempHealthBar.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TempHealthBar : MonoBehaviour {
+
+    public Slider HealthBar;
+    public Text Health;
+    public int currentHealth;
+    public int maxHealth;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Health.text = currentHealth.ToString();
+        HealthBar.value = ((float)currentHealth / (float)maxHealth);
+    }
+}
