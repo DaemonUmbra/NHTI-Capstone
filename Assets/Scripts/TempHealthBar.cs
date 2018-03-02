75f308ab93fd8cf267a42b2e5e4920d8546ad72b
blaj95 <32546217+blaj95@users.noreply.github.com>
2018-03-02 15:36:07 -0500

    new ui fix

--- Assets/Scripts/TempHealthBar.cs
+++ Assets/Scripts/TempHealthBar.cs
@@ -7,16 +7,16 @@ public class TempHealthBar : MonoBehaviour {
 
     public Slider HealthBar;
     public Text Health;
+    private PlayerStats pstats;
 
-
-	// Use this for initialization
-	void Start () {
-		
-	}
+    // Use this for initialization
+    void Start () {
+        pstats = GetComponent<PlayerStats>();
+    }
 	
 	// Update is called once per frame
 	void Update () {
-        Health.text = GetComponent<PlayerStats>().CurrentHp.ToString();
+        Health.text = pstats.CurrentHp.ToString();
         HealthBar.value = ((float)GetComponent<PlayerStats>().CurrentHp / (float)GetComponent<PlayerStats>().MaxHp);
     }
 }
