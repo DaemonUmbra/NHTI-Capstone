f386f372adab74f9bdc7114b57ca2b515e55c4a7
EmberCelicaX <33268735+EmberCelicaX@users.noreply.github.com>
2018-03-23 14:27:54 -0400

    Fireball Powerup (WIP)

---
+++ Assets/Scripts/ApplyBurn.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ApplyBurn : Projectile
+{
+    public new void Start()
+    {
+        base.Start();
+    }
+
+    public new void Update()
+    {
+        base.Update();
+    }
+    private void OnTriggerEnter(Collider collision)
+    {
+        GameObject hit = collision.gameObject;
+        PhotonView hitView = hit.GetPhotonView();
+        PlayerStats hitStats = hit.GetComponent<PlayerStats>();
+        // Verify hit photon view
+        if (hitView)
+        {
+            // Make sure the bullet isn't hitting it's own player
+            if (hitView.owner != photonView.owner && hitStats && photonView.isMine)
+            {
+                // Apply damage to the player
+                hitStats.TakeDamage(damage);
+
+                print("Player hit by fireball!");
+                PhotonNetwork.Destroy(photonView);
+            }
+        }
+    }
+}
