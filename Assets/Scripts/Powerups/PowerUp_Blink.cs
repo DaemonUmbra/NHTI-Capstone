0551249558fcc394a1872f106d99a3aa3982f5d2
blaj95 <32546217+blaj95@users.noreply.github.com>
2018-03-27 15:14:03 -0400

    Blink working
    blink powerup is functional

--- Assets/Scripts/Powerups/PowerUp_Blink.cs
+++ Assets/Scripts/Powerups/PowerUp_Blink.cs
@@ -13,7 +13,9 @@ namespace Powerups
         [HideInInspector]
         public PlayerController playercontrol;
 
-        public float BlinkDistance; // For debugging purposes. Once this has been determined, will be set to HideInInspector
+        private PlayerShoot pShoot;
+
+        public float BlinkDistance = 12; // For debugging purposes. Once this has been determined, will be set to HideInInspector
                                     
 
         // Use this for initialization
@@ -26,16 +28,27 @@ namespace Powerups
             // Set name
             Name = "Blink";
             Debug.Log(Name + " Added");
-            Cooldown = 5.0f;
+            Cooldown = 2.0f;
             playercontrol = GetComponent<PlayerController>();
-           
 
+            pShoot = GetComponent<PlayerShoot>();
+            if (pShoot)
+            {
+
+                pShoot.shoot += TryActivate;
+            }
             // Call base function
             base.OnAbilityAdd();
         }
 
         public override void OnAbilityRemove()
         {
+            if (pShoot)
+            {
+                pShoot.shoot -= TryActivate;
+            }
+            pShoot = null;
+
             // Call base function
             base.OnAbilityRemove();
         }
@@ -48,17 +61,37 @@ namespace Powerups
 
         protected override void Activate()
         {
-            Debug.Log("Blink blink");
+            Debug.Log("Blink!");
             // Checks if player is blocked by a wall or player, only allows activate if the raycast returns false
-            if (Physics.Raycast(transform.position, transform.TransformDirection(transform.forward), BlinkDistance) == false)
+            if (photonView.isMine)
             {
-                transform.position += transform.forward * BlinkDistance;
-                // **Cooldown**
+                Vector3 destination = transform.position + transform.forward * BlinkDistance;
+                RaycastHit hit;
+                if (Physics.Linecast(transform.position, destination, out hit))
+                {
+                    destination = transform.position + transform.forward * (hit.distance - 1.5f);
+                }
 
+                destination.y = 1000;
+                if (Physics.Raycast(destination, -Vector3.up, out hit))
+                {
+                    destination = hit.point;
+                    destination.y += 2;
+                    transform.position = destination;
+                }
+                //    if (Physics.Raycast(transform.position, transform.TransformDirection(transform.forward), BlinkDistance) == false)
+                //    {
+                //        if (photonView.isMine)
+                //        {
+                //            transform.position = transform.forward * BlinkDistance;
+                //        }
+
+                //        **Cooldown * *
+
+                //}
             }
-
-            base.Activate();
-            Debug.Log("Activate Test - Blink");
+                base.Activate();
+            
         }
 
     }
