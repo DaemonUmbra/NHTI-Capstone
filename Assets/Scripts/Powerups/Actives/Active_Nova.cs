80d92ec0cf6e988afe7197a141295e370b3f6449
DaemonUmbra <daemon.umbra@gmail.com>
2018-04-06 15:05:10 -0400

    Try this Brodey

--- Assets/Scripts/Powerups/Actives/Active_Nova.cs
+++ Assets/Scripts/Powerups/Actives/Active_Nova.cs
@@ -24,8 +24,6 @@ namespace Powerups {
             Tier = PowerupTier.Rare;
         }
 
-
-
         public override void OnAbilityAdd()
         {
             base.OnAbilityAdd();
--- Assets/Scripts/Util/ModelManager.cs
+++ Assets/Scripts/Util/ModelManager.cs
@@ -7,67 +7,118 @@ public class ModelManager : Photon.MonoBehaviour
 {
     PlayerStats pStats;
     private Dictionary<string, Transform> modelRegistry = new Dictionary<string, Transform>();
+    private Dictionary<string, Transform> subModelRegistry = new Dictionary<string, Transform>();
+    private List<string> activeSubModels = new List<string>();
+    private string currentModel = "Default";
+
+    public delegate void VoidDelegate();
+
+    public class ModelChangeEventArgs: EventArgs
+    {
+        public string oldModelName;
+        public Transform oldModel;
+        public string newModelName;
+        public Transform newModel;
+    }
+
+    private void OnModelChanged(string newModel, string oldModel)
+    {
+        EventHandler<ModelChangeEventArgs> handler = ModelChanged;
+        ModelChangeEventArgs args = new ModelChangeEventArgs();
+        args.oldModelName = oldModel;
+        args.newModelName = newModel;
+        args.newModel = modelRegistry[newModel];
+        args.oldModel = modelRegistry[oldModel];
+        ModelChanged(this, args);
+    }
+
+    public event EventHandler<ModelChangeEventArgs> ModelChanged;
 
     // Awake is called when the script instance is being loaded
     private void Awake()
     {
         pStats = GetComponent<PlayerStats>();
         LoadModels();
+        LoadSubModels();
     }
 
     private void LoadModels()
     {
         modelRegistry.Add("Default", Resources.Load<Transform>("PlayerModels/Default"));
-       // modelRegistry.Add("Gun", Resources.Load)
         modelRegistry.Add("Aliens", Resources.Load<Transform>("PlayerModels/UFO"));
         modelRegistry.Add("Beam", Resources.Load<Transform>("PlayerModels/DefaultBeam"));
     }
 
+    private void LoadSubModels()
+    {
+        subModelRegistry.Add("Beam", Resources.Load<Transform>("PlayerModels/Beam"));
+    }
+
     public void SetModel(string modelName)
     {
-        bool ScaleAdjusted = false;
         if (photonView.isMine)
         {
             Transform temp;
             if (modelRegistry.TryGetValue(modelName, out temp))
             {
-                //if(pStats.HasTransform(transform.Find("Player Model")))
-                //{
-                //    ScaleAdjusted = true;
-                //}
-                photonView.RPC("MM_SetModel", PhotonTargets.All, modelName,ScaleAdjusted);
-            }
-            else if (modelRegistry.TryGetValue("Beam", out temp))
-            {
-                photonView.RPC("Beam_SetModel", PhotonTargets.All, modelName);
+                photonView.RPC("MM_SetModel", PhotonTargets.All, modelName);
+                OnModelChanged(modelName,currentModel);
+                currentModel = modelName;
             }
         }
     }
 
     [PunRPC]
-    public void MM_SetModel(string modelName, bool scaleAdjusted)
+    public void MM_SetModel(string modelName)
     {
-        if (scaleAdjusted)
-        {
-            //pStats.RemoveTransform(transform.Find("Player Model"));
-        }
         Debug.Log(photonView.owner + " requests change to model: " + modelName);
         Transform CurrentModel = transform.Find("Player Model");
         Destroy(CurrentModel.gameObject);
         Transform model = Instantiate(modelRegistry[modelName],transform);
         model.name = "Player Model";
-        if (scaleAdjusted)
+    }
+
+    public void AddSubModel(string subModelName)
+    {
+        if (photonView.isMine)
         {
-            //pStats.AddTransform(model);
+            Transform temp;
+            if (subModelRegistry.TryGetValue(subModelName, out temp))
+            {
+                photonView.RPC("MM_AddSubModel", PhotonTargets.All, subModelName);
+                activeSubModels.Add(subModelName);
+            }
         }
     }
+
     [PunRPC]
-    public void Beam_SetModel(string modelName)
+    public void MM_AddSubModel(string subModelName)
     {
-        Debug.Log(photonView.owner + " requests change to model: " + modelName);
+        Debug.Log(photonView.owner + " requests addition of " + subModelName + " to their model");
         Transform CurrentModel = transform.Find("Player Model");
-        Destroy(CurrentModel.gameObject);
-        Transform model = Instantiate(modelRegistry[modelName], transform);
-        model.name = "Beam";
+        Transform subModel = Instantiate(subModelRegistry[subModelName], CurrentModel);
+        subModel.name = subModelName;
+    }
+
+    public void RemoveSubModel(string subModelName)
+    {
+        if (photonView.isMine)
+        {
+            photonView.RPC("MM_RemoveSubModel", PhotonTargets.All, subModelName);
+        }
+    }
+
+    [PunRPC]
+    public void MM_RemoveSubModel(string subModelName)
+    {
+        if (activeSubModels.Contains(subModelName))
+        {
+            Transform CurrentModel = transform.Find("Player Model");
+            Transform submodel = CurrentModel.Find(subModelName);
+            if (submodel != null)
+            {
+                Destroy(submodel);
+            }
+        }
     }
 }
