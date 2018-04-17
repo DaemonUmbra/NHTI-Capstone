2f48af7b8f1f0f3b359c57a346c9542027b846c1
DaemonUmbra <daemon.umbra@gmail.com>
2018-04-17 14:53:52 -0400

    Fixed a bug where Nova was too dependent on the player and would break if the player died mid-explosion

--- Assets/Scripts/Powerups/NovaDummy.cs
+++ Assets/Scripts/Powerups/NovaDummy.cs
@@ -1,15 +1,23 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
-public class NovaDummy : MonoBehaviour
+public class NovaDummy : Photon.MonoBehaviour
 {
-    Transform Detonator;
-    Powerups.Active_Nova Powerup;
+    private List<Transform> Affected = new List<Transform>();
+    private Vector3 ScaleStepVector;
+    public float ExplosionSize = 15f;
+    public float ExplosionTime = 1f;
+    public float ScaleStep = 1f;
+    public float ExplosionForce = 17f;
+    public float ExplosionDamage = 75f;
 
     // Use this for initialization
     void Start()
     {
-        Powerup = Detonator.GetComponent<Powerups.Active_Nova>();
+        ScaleStepVector = new Vector3(ScaleStep, ScaleStep, ScaleStep);
+        transform.localScale = Vector3.zero;
+        StartCoroutine(Explode(ExplosionSize,ExplosionTime));
     }
 
     // Update is called once per frame
@@ -18,15 +26,48 @@ public class NovaDummy : MonoBehaviour
 
     }
 
-    public void SetDetonator(Transform transform)
+    IEnumerator Explode(float size, float time)
     {
-        Detonator = transform;
+        bool Exploding = true;
+        bool Stage1 = true;
+        while (Exploding)
+        {
+            if (transform.localScale.magnitude < size && Stage1)
+            {
+                //Grow
+                transform.localScale += ScaleStepVector;
+            }
+            else
+            {
+                if (transform.localScale == Vector3.zero)
+                {
+                    Affected.Clear();
+                    Destroy(gameObject);
+                }
+                if (transform.localScale.magnitude >= size) { Stage1 = false; }
+                //Shrink
+                transform.localScale -= ScaleStepVector;
+            }
+            yield return new WaitForEndOfFrame();
+        }
     }
+
     private void OnTriggerEnter(Collider other)
     {
-        if(Powerup != null)
+        if (photonView.isMine)
         {
-            Powerup.OnHitPlayer(other.transform);
+            if (!Affected.Contains(other.transform))
+            {
+                if (other.GetComponent<PlayerStats>() != null)
+                {
+                    other.GetComponent<PlayerStats>().TakeDamage(ExplosionDamage, gameObject);
+                    Affected.Add(other.transform);
+                }
+                if (other.GetComponent<Rigidbody>() != null)
+                {
+                    other.GetComponent<Rigidbody>().AddExplosionForce(ExplosionForce, transform.position, ExplosionSize);
+                }
+            }
         }
     }
 }
