330fe2cdef642ecd10f9ade765b0effb4798cba7
blaj95 <32546217+blaj95@users.noreply.github.com>
2018-04-18 03:15:10 -0400

    one works. the other doesnt

--- Assets/Scripts/Powerups/Actives/Active_ThunderWave.cs
+++ Assets/Scripts/Powerups/Actives/Active_ThunderWave.cs
@@ -74,8 +74,6 @@ namespace Powerups
 
                 if (hit.rigidbody != null && hit.transform.gameObject.tag == "Player" && !hit.transform.gameObject.GetComponent<PhotonView>().isMine)
                 {
-                    if (hit.transform.gameObject != this.gameObject)
-                    {
                         Vector3 hitDir = hit.point;
                         Vector3 forceDir = Quaternion.AngleAxis(0, hitDir) * Vector3.up;
                         hit.rigidbody.AddForce(forceDir * upforce, ForceMode.Impulse);
@@ -86,7 +84,7 @@ namespace Powerups
                         PlayerStats stats;
                         stats = hit.transform.GetComponent<PlayerStats>();
                         stats.TakeDamage(5, gameObject);
-                    }
+                    
                 }
 
             }
