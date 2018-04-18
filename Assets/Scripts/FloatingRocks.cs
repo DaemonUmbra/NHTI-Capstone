33f881ff6e4d88599450faac5675d0a3ac6c4f8b
Cobaltmoon <32678490+Cobaltmoon@users.noreply.github.com>
2018-04-18 03:22:49 -0400

    added plaeyr csnvas

--- Assets/Scripts/FloatingRocks.cs
+++ Assets/Scripts/FloatingRocks.cs
@@ -2,14 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[ExecuteInEditMode]
+//[ExecuteInEditMode] coolest shit ever
 
 public class FloatingRocks : MonoBehaviour
 {
     public float speed = 5f;
     public float rateOfFire = -1.0f;
     float nextFire = 1.0f;
-    
+    Vector3 axis = Vector3.up;
 
     // Use this for initialization
     void Start ()
@@ -20,15 +20,16 @@ public class FloatingRocks : MonoBehaviour
 	// Update is called once per frame
 	void Update ()
     {
-        Vector3 randomDirection = new Vector3(Random.value, Random.value, Random.value);
-
+        
+        
         nextFire = Time.time + rateOfFire;
 
         if (Time.time >= nextFire)
         {
-            transform.Rotate(randomDirection, speed * Time.deltaTime);
+            Vector3 randomDirection = new Vector3(Random.value, Random.value, Random.value);
+            axis = randomDirection;
         }
-    
-    
+
+        transform.Rotate(axis, speed * Time.deltaTime);
     }
 }
