ad765afb60fe0f91cc326d6e18af4a10f657bcf2
Cobaltmoon <32678490+Cobaltmoon@users.noreply.github.com>
2018-04-20 15:53:52 -0400

    need changes to rand seed

--- Assets/Scripts/FloatingRocks.cs
+++ Assets/Scripts/FloatingRocks.cs
@@ -4,32 +4,40 @@ using UnityEngine;
 
 //[ExecuteInEditMode] //coolest shit ever
 
+
+    // Seed my own RNG
 public class FloatingRocks : MonoBehaviour
 {
     public float speed = 5f;
-    public float rateOfFire = -1.0f;
+    public float rate; 
     float nextFire = 1.0f;
-    Vector3 axis = Vector3.up;
+    public Vector3 axis;
+    public Vector3 randomDirection;
 
     // Use this for initialization
     void Start ()
     {
         
 	}
-	
-	// Update is called once per frame
-	void Update ()
+
+    private void Awake()
+    {
+        rate = Random.value;
+    }
+    // Update is called once per frame
+    void Update ()
     {
         
+
+        nextFire = Time.time + rate;
+
+            if (Time.time >= nextFire)
+            {
+                randomDirection = new Vector3(Random.Range(0f,1f), Random.Range(0f, 1f), Random.Range(0f, 1f);
+                axis = randomDirection;
+            }
+
+            transform.Rotate(axis, speed * Time.deltaTime);
         
-        nextFire = Time.time + rateOfFire;
-
-        if (Time.time >= nextFire)
-        {
-            Vector3 randomDirection = new Vector3(Random.value, Random.value, Random.value);
-            axis = randomDirection;
-        }
-
-        transform.Rotate(axis, speed * Time.deltaTime);
     }
 }
