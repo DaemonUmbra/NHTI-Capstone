3933820948499e46f474d829e4bcacea35058d88
Cobaltmoon <32678490+Cobaltmoon@users.noreply.github.com>
2018-04-24 16:55:31 -0400

    rock float

--- Assets/Scripts/FloatingRocks.cs
+++ Assets/Scripts/FloatingRocks.cs
@@ -8,36 +8,62 @@ using UnityEngine;
     // Seed my own RNG
 public class FloatingRocks : MonoBehaviour
 {
-    public float speed = 5f;
-    public float rate; 
-    float nextFire = 1.0f;
-    public Vector3 axis;
-    public Vector3 randomDirection;
-
-    // Use this for initialization
-    void Start ()
-    {
-        
-	}
-
     private void Awake()
     {
-        rate = Random.value;
+        
     }
-    // Update is called once per frame
-    void Update ()
+
+    public Vector3 rotateVec = Vector3.up;
+    public float speed = 45.0f;
+
+    private void Update()
     {
-        
+        transform.Rotate(Vector3.up, speed * Time.deltaTime);//Yaw
+        transform.Rotate(Vector3.forward, speed * Time.deltaTime);//Roll
+        transform.Rotate(Vector3.left, speed * Time.deltaTime);//Pitch
 
-        nextFire = Time.time + rate;
-
-            if (Time.time >= nextFire)
-            {
-                randomDirection = new Vector3(Random.Range(0f,1f), Random.Range(0f, 1f), Random.Range(0f, 1f));
-                axis = randomDirection;
-            }
-
-            transform.Rotate(axis, speed * Time.deltaTime);
-        
     }
+
+
+
 }
+
+
+//   public GameObject[] floatingObjs;
+
+//   public float speed = 5f;
+//   public float rate; 
+//   float nextFire = 1.0f;
+//   public Vector3 axis;
+//   public Vector3 randomDirection;
+
+//   // Use this for initialization
+//   void Start ()
+//   {
+
+//}
+
+//   private void Awake()
+//   {
+//       rate = Random.value;
+//      // if (floatingObjs == null)
+//           floatingObjs = GameObject.FindGameObjectsWithTag("floating");
+//   }
+//   // Update is called once per frame
+//   void Update ()
+//   {       
+//       nextFire = Time.time + rate;
+
+//       for (int i = 0; i < floatingObjs.Length; i++ )
+//       {
+//           Random.seed += (i);// i*i\
+
+//           if (Time.time >= nextFire)
+//           {
+//               randomDirection = new Vector3(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f));
+//               axis = randomDirection;
+//           }
+
+//           floatingObjs[i].transform.Rotate(axis, speed);//* Time.deltaTime);
+//       }        
+//   }
