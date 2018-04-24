6e9daa0f798b8835dc649c04fafc6dbb8ac3c684
IanFC <33268732+IanFC@users.noreply.github.com>
2018-04-24 14:43:16 -0400

    slime and stuff

--- Assets/Scripts/Other/SlimeBall.cs
+++ Assets/Scripts/Other/SlimeBall.cs
@@ -4,18 +4,25 @@ public class SlimeBall : Projectile
 {
     Vector3 direction;
     GameObject plr;
+    float force = 20;
+    private void Start()
+    {
+        Rigidbody rb = GetComponent<Rigidbody>();
 
+        rb.velocity = (transform.forward + (transform.up / 5)) * force;
+    }
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.tag == "Player")
         {
             direction = (collision.transform.position - transform.position);
             //direction = direction.normalized;
-            direction.y = Mathf.Abs(direction.y);
+            // direction.y = Mathf.Abs(direction.y);
+            direction.y = 1;
             plr = collision.gameObject;
             PlayerController hitController = plr.GetComponent<PlayerController>();
             Vector3 mult = Vector3.one;
-            mult.y = 1f;
+            mult.y = .7f;
             mult.x = mult.z = .5f;
 
             // Only knockback on master client, position is synced
