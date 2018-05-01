7a5ff77e9643346d264c652def888d7759e96357
bnelligan <nelligan81@gmail.com>
2018-05-01 18:30:43 -0400

    Pickup serialize fix

--- Assets/Scripts/Powerups/Powerup System/Powerup Spawning/Pickup.cs
+++ Assets/Scripts/Powerups/Powerup System/Powerup Spawning/Pickup.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using Powerups;
 
-public class Pickup : Photon.PunBehaviour
+public class Pickup : Photon.MonoBehaviour
 {
     private void OnTriggerEnter(Collider other)
     {
